ff89fcf398d4b7c188d53bb4bff237feb3d9cb6c
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-10-23 12:35:15 +0200

    Zwischenstand

--- app/scripts/controllers/bookListCtrl.js
+++ app/scripts/controllers/bookListCtrl.js
@@ -1,3 +1,27 @@
 angular.module('dpunktApp').controller('bookListCtrl', function($scope) {
 
+  $scope.books = [
+    {
+      title: 'AngularJS',
+      subtitle: 'Eine Einführung in das JS-Framework',
+      author: 'pt, rb',
+      isbn: '123-456-789',
+      numPages: 360
+    },
+    {
+      title: 'JavaScript Effektiv',
+      subtitle: 'Effektives JS',
+      author: 'foo',
+      isbn: '111-222-333',
+      numPages: 250
+    },
+    {
+      title: 'TypeScript',
+      subtitle: 'Ein netter Versuch von Microsoft',
+      author: 'bar',
+      isbn: '222-222-222',
+      numPages: 400
+    }
+  ];
+
 });
