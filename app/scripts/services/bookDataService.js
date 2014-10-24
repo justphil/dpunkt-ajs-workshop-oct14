2fb83c7259e7392d22be5321a351a5d510424562
Philipp Tarasiewicz <philipp.tarasiewicz@googlemail.com>
2014-10-24 13:49:26 +0200

    Zwischenstand

--- app/scripts/services/bookDataService.js
+++ app/scripts/services/bookDataService.js
@@ -1,40 +1,10 @@
-angular.module('dpunktApp').factory('bookDataService', function($q, dataEnhancer) {
+angular.module('dpunktApp').factory('bookDataService', function($q, $http) {
 
-  var books = [
-    {
-      title: 'AngularJS',
-      subtitle: 'Eine Einführung in das JS-Framework',
-      author: 'pt, rb',
-      isbn: '123-456-789',
-      numPages: 360
-    },
-    {
-      title: 'JavaScript Effektiv',
-      subtitle: 'Effektives JS',
-      author: 'foo',
-      isbn: '111-222-333',
-      numPages: 250
-    },
-    {
-      title: 'TypeScript',
-      subtitle: 'Ein netter Versuch von Microsoft',
-      author: 'bar',
-      isbn: '222-222-222',
-      numPages: 400
-    }
-  ];
+  var baseUrl = 'http://ajs-workshop.herokuapp.com/api';
 
   // private implementation
   function getBooks() {
-    var booksCopy = angular.copy(books);
-
-    booksCopy.forEach(function(b) {
-      dataEnhancer.enhance(b);
-    });
-
-    return $q.when({
-      data: booksCopy
-    });
+    return $http.get(baseUrl + '/books');
   }
 
   function getBookByIsbn(isbn) {
